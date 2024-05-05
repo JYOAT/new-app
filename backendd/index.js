d9af7a3fcfe78146579780d2002eaccc57f83452
Jyotsna Kumar <atwaljyotsna@gmail.com>
2024-05-05 13:34:26 +0530

    backened done

--- backendd/index.js
+++ backendd/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {createTodo, updateTodo}= require("./types");
+const { todo } = require("./db");
 const app = express();
 app.use(express.json());
 
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success){
@@ -12,17 +13,32 @@ app.post("/todo", function (req, res) {
         })
         return;
     }
+    //put it in db
+    await todo.create({
+        title: createPayload.title,
+        description : createPayload.description,
+        completed : false
+    });
+
+    res.json({
+        msg: "Todo created"
+    });
    
 
 
 
 });
 
-app.get("/todos", function (req, res) {
+app.get("/todos", async function (req, res) {
+    const todos = await todo.find();
+
+    res.json({
+        todos
+    })
 
 });
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updatePayload.safeParse(updatePayload);
     if (!parsedPayload.success){
@@ -31,6 +47,17 @@ app.put("/completed", function (req, res) {
         })
         return;
     }
+
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed : true
+    })
+
+    res.json({
+        msg : "todo marked as completed"
+    })
+
 
 
 });
