dd4383b057a84e6a803cbbccada3381d08dd93c5
Jyotsna Kumar <atwaljyotsna@gmail.com>
2024-05-16 11:38:19 +0530

    todo by react

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,19 @@
 export function CreateTodo(){
     return <div>
-        <input type="text" placeholder="title"/><br />
-        <input type="text" placeholder="description"/><br />
-        <button>Add a Todo</button>
+        <input style={{
+            padding:10,
+            margin:10
+
+        }} type="text" placeholder="title"/><br />
+        
+        <input style={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="description"/><br />
+        
+        <button style={{
+            padding:10,
+            margin:10
+        }}>Add a Todo</button>
     </div>
 }
