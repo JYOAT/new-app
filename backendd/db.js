cdeff71ebdb2bb1021ddd134770fadc245bdf750
Jyotsna Kumar <atwaljyotsna@gmail.com>
2024-05-16 11:37:27 +0530

    updated db file

--- backendd/db.js
+++ backendd/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const { string, boolean } = require("zod");
-mongoose.connect("mongodb+srv://jyotsna2003:JJyotsna20032003@@@atlascluster.4htwpzx.mongodb.net/");
+mongoose.connect("mongodb+srv://jyotsna2003:JJyotsna20032003%40%40@atlascluster.4htwpzx.mongodb.net/todos");
 const todoSchema = mongoose.Schema({
-    title : string,
-    description : string,
-    completed : boolean
+    title : String,
+    description : String,
+    completed : Boolean
 })
 
-const todo = mongoose.model('todo',todoSchema);
+const todo = mongoose.model('todos',todoSchema);
 module.exports={
     todo : todo
 };
